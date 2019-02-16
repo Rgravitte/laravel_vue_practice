8d19b94708489aaccb02a1401d3c593012d9e979
Ross Gravitte <rgravitte@Rosss-MacBook-Pro.local>
2019-02-16 06:52:50 -0500

    SPA working with vue/laravel

--- frontend/src/views/Articles.vue
+++ frontend/src/views/Articles.vue
@@ -1,14 +1,41 @@
 <template>
   <div class="about">
-    <h1>Articles</h1>
+    <div v-for="item in articles" :key="item.id">
+      <h1>{{ item.title }}</h1>
+      <p>{{ item.body }}</p>
+    </div>
   </div>
 </template>
 <script>
+  import axios from 'axios'
+    export default{
+        name: 'articles',
+        data() {
+          return{
+            articles: null
+          }
+        },
+        created() {
+          this.getArticles()
+        },
+        methods: {
+          getArticles() {
+            axios
+            .get('/api/articles')
+
+            .then( res => {
+              const {data:{data}} = res
+              this.articles = data
+              console.log(res)
+            })
+
+            .catch(err => {
+              console.log(err.message)
+            })
+          }
+        }
+    }
+
 
 </script>
-    export default{
-        name: 'articles'
-    }
-<style>
 
-</style>
